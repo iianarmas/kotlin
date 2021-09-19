58aa047e423c1cc9d01259a247db3044c5612fe5
iian armas <armas.cav@gmail.com>
2021-09-20 05:41:13 +0800

    Create basicFunctions.kt

---
+++ basicFunctions.kt
@@ -0,0 +1,17 @@
+// fun getName(): String {
+    // return "Hello Kotlin"
+// }
+
+// or
+// fun getname(): String = "Hello Kotlin"
+
+// or
+
+fun getName() = "Hello Kotlin"
+
+fun sayHello() {
+    println(getName())
+}
+
+fun main() {
+    sayHello()
