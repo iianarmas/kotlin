24d714a6b9a97885eb844eea6836a94050eade46
iian armas <armas.cav@gmail.com>
2021-09-20 05:15:10 +0800

    Create when.kt

---
+++ when.kt
@@ -0,0 +1,16 @@
+var age = 7
+var name: String? = null
+
+
+fun main() {
+    when (age) {
+       1 -> println("One")
+       2 -> println("Two")
+       3 -> println("Three")
+       4 -> println("Four")
+       5 -> println("Five")
+       6 -> println("Six")
+       else -> println("No match")
+    }
+
+}
